57b1b0626e99aa461b04593c12bee760463882e5
JBE29 <jeanbaptiste.emily@gmail.com>
2022-10-03 15:55:08 +0200

    last commit

--- exo_12.rb
+++ exo_12.rb
@@ -1,5 +1,10 @@
 puts "Bonjour"
 puts "Voici le dernier exercice du jour"
-5.times do |i|
+puts "On va jouer à un jeu, donne moi un nombre stp"
+print "> "
+number = gets.to_i
+puts "Et maintenant je vais compter jusque là"
+
+number.times do |i|
     puts i + 1
   end
