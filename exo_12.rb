e7e56140541257dc0572d026828cd5e3712632df
JBE29 <jeanbaptiste.emily@gmail.com>
2022-10-03 16:17:20 +0200

    small changes on exo12

--- exo_12.rb
+++ exo_12.rb
@@ -4,7 +4,17 @@ puts "On va jouer à un jeu, donne moi un nombre stp"
 print "> "
 number = gets.to_i
 puts "Et maintenant je vais compter jusque là"
+puts "Je commence quand tu dis OK"
+print "> "
+validation = gets.to_i
+
+if (validation = "OK") 
+    then
+    
 
 number.times do |i|
     puts i + 1
   end
+else 
+    puts "Je commence quand tu dis OK" 
+end
